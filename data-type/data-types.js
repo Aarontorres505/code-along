48f8e567b18bba28021e970d4b876deb3f5f476c
aaron torres <atorres205@cnm.edu>
2021-10-06 10:22:28 -0600

    code-along

---
+++ data-type/data-types.js
@@ -0,0 +1,18 @@
+// string
+const name  = 'fred'
+const anotherName = "Jim"
+const yetAnotherName = `Mary and ${name}`
+
+// number
+const age = 18
+
+const Zipcode = '87109'
+
+const cars = ['ford','dodge', 'toyota']
+const empty = []
+console.log(cars[0])
+console.log(cars[2])
+
+// Object ....JSON
+const person = {name: 'Fred', age: 18}
+
